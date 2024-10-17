94805a3fdc0b5898e832d55b5ffd7c6c648f4cd5
Atreya Tadepalli <atreya.tadepalli@duke.edu>
2024-10-17 12:53:59 -0400

    second database

--- mylib/extract.py
+++ mylib/extract.py
@@ -20,4 +20,14 @@ def extract(url="https://raw.githubusercontent.com/\
     return file_path
 
 
-
+def extract_second(url="https://gist.githubusercontent.com/xav\
+                   ier-moreno/5cc951dce9af4c6c6655/raw/feb35d621ac8c\
+                   bf20bfd36266fbd779e22016158/MLB.csv", 
+            file_path="MLB.csv",
+            directory="data",
+):
+    """"Extract a url to a file path"""
+    with requests.get(url) as r:
+        with open(file_path, 'wb') as f:
+            f.write(r.content)
+    return file_path
--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -59,3 +59,50 @@ def load(dataset="mlb-test.csv"):
     conn.close()
     return "baseball.db"
 
+
+def load_second(dataset="MLB.csv"):
+    """"Transforms and Loads data into the local SQLite3 database"""
+
+    #prints the full working directory and path
+    print(os.getcwd())
+    payload = csv.reader(open(dataset, newline=''), delimiter=',')
+    next(payload)
+    filtered_payload = [row[:3] for row in payload]
+
+    #for row in filtered_payload:
+        #try:
+            #row[3] = int(row[3])
+        #except ValueError:
+            #row[3] = None  # Set to None or some other value if not an integer
+
+    conn = sqlite3.connect('baseball_2.db')
+    c = conn.cursor()
+    c.execute("DROP TABLE IF EXISTS baseball_2")
+    #c.execute("CREATE TABLE baseball\
+    #(team,W,L,GB,year,League)")
+    c.execute(
+        """
+        CREATE TABLE baseball_2 (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Team TEXT,
+            Salary INTEGER,
+            Winning INTEGER
+        )
+        """
+    )
+    #insert
+    c.executemany(
+        """
+        INSERT INTO baseball_2 (
+            Team, 
+            Salary, 
+            Winning
+        ) 
+        VALUES (?, ?, ?)
+        """,
+        filtered_payload,
+    )
+    conn.commit()
+    conn.close()
+    return "baseball_2.db"
+
